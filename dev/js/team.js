afbc7f511dacdaebec0591afc4038ee33275bd22
github KEN0817 <github friday820817@gmail.com>
2019-08-29 22:28:48 +0800

    NO RWD

--- dev/js/team.js
+++ dev/js/team.js
@@ -6,6 +6,7 @@ function join() {
     // console.log(1);
     $id("join").style.display='';
     $id("joined").style.display='none';
+    $id("create").style.display='none';
     $id("team_join").classList.add('team_option_select');
     $id("team_joined").classList.remove('team_option_select');
     $id("team_create").classList.remove('team_option_select');
@@ -24,6 +25,14 @@ function joinInvitation(e){
     $id("no").addEventListener("click",()=>{
         $id("team_join_window").style.display="none";
     },false);
+    $id("ok").addEventListener("click",()=>{
+        this.remove();
+        $id("team_join_window").style.display="none";
+        $id("joined").append(this);
+    },false);
+    // var addjoined=this;
+    // console.log(this);
+    
     // $id("ok").innerText='加入團隊';
     // $id("no").innerText='我不要';
     // console.log(this);
@@ -31,6 +40,7 @@ function joinInvitation(e){
 
 function joined() {
     $id("join").style.display='none';
+    $id("create").style.display='none';
     $id("joined").style.display='';
     $id("team_join").classList.remove('team_option_select');
     $id("team_joined").classList.add('team_option_select');
@@ -42,16 +52,22 @@ function joined() {
     $id("no").innerText='我不想';
 }
 function dropOut(e){
+    console.log(this);
     $id("team_join_window").style.opacity=1;
     $id("team_join_window").style.display="";
     $id("no").addEventListener("click",()=>{
         $id("team_join_window").style.display="none";
     },false);
-    // $id("ok").innerText='退出團隊';
-    // $id("no").innerText='我不想';
+    $id("ok").addEventListener("click",()=>{
+        this.remove();
+        $id("team_join_window").style.display="none";
+    },false);
 }
 
 function create() {
+    $id("join").style.display='none';
+    $id("joined").style.display='none';
+    $id("create").style.display='';
     $id("team_join").classList.remove('team_option_select');
     $id("team_joined").classList.remove('team_option_select');
     $id("team_create").classList.add('team_option_select');
@@ -62,5 +78,6 @@ window.addEventListener("load", function () {
     $id("team_joined").addEventListener("click", joined, false);
     $id("team_create").addEventListener("click", create, false);
     join();
+    // create();
     $id("team_join_window").style.display="none";
 }, false);
